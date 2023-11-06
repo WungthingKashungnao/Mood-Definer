3a6f8ec94f6a64bc87fc18f3cdbf421c139c041f
wungthing <wungthingkashungnao123@gmail.com>
2023-11-06 18:35:01 +0530

    made it working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import "./app.css";
+import useSound from "use-sound";
+import sounds from "./audios/sounds";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [randomSound, setRandomSound] = useState(0); //state to select random sound from array
+  const [playSound] = useSound(sounds[randomSound]);
+
+  useEffect(() => {
+    playSound();
+    console.log(sounds.length);
+  }, [randomSound]);
+
+  const hanldePlay = () => {
+    let anySound = Math.floor(Math.random() * 19);
+    setRandomSound(anySound);
+  };
+  return (
+    <div className="App">
+      <button className="btn" onClick={hanldePlay}>
+        Set The Vibe!
+      </button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/audios/sounds.js
@@ -0,0 +1,42 @@
+import confirmation from "./confirmation.mp3";
+import ara from "./ara-ara-sound-effect-127279.mp3";
+import brass from "./brass-144755.mp3";
+import epic from "./epic-01-voiced-165994.mp3";
+import evil from "./evil-witch-laugh-140135.mp3";
+import fail from "./fail-144746.mp3";
+import fart from "./fart-145914.mp3";
+import funny from "./funny-reaction-120318.mp3";
+import isnt from "./isnt-that-amazing-meme-sfx-172488.mp3";
+import laughing from "./laughing-man-117725.mp3";
+import man from "./man-scream-121085.mp3";
+import mario from "./mario-grabish-14183.mp3";
+import menx27s from "./menx27s-laughter-121577.mp3";
+import meow from "./meow-to-an-egg-36412.mp3";
+import ooh from "./ooh-123103.mp3";
+import scream from "./scream-121575.mp3";
+import uwu from "./uwu-sound-119010.mp3";
+import veryInfectiousLaughter from "./very-infectious-laughter-117727.mp3";
+import wow from "./wow-121578.mp3";
+
+const sounds = [
+  confirmation,
+  ara,
+  brass,
+  epic,
+  evil,
+  fail,
+  fart,
+  funny,
+  isnt,
+  laughing,
+  man,
+  mario,
+  menx27s,
+  ooh,
+  scream,
+  uwu,
+  veryInfectiousLaughter,
+  wow,
+  meow,
+];
+export default sounds;
